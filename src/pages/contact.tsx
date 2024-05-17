9e6052ece0ab84b5e65541876af8004ef68371ad
user <chinedu.go@gmail.com>
2024-05-17 11:44:12 +0100

    update

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -57,11 +57,11 @@ const Contact = () => {
 							possible. Finally, if you prefer to connect on
 							social media, you can find me on{" "}
 							<a
-								href={INFO.socials.instagram}
+								href={INFO.socials.linkedin}
 								target="_blank"
 								rel="noreferrer"
 							>
-								{INFO.socials.instagram}
+								{INFO.socials.linkedin}
 							</a>
 							. I post regular updates and engage with my
 							followers there, so don't hesitate to reach out.
